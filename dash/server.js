64cc7eb77f5641e3c87d379e2a1a02b490b69a97
Justin Swanson <geeksunny@gmail.com>
2018-02-04 18:14:52 -0600

    Moving lightswitch setup to an external json config

--- dash/server.js
+++ dash/server.js
@@ -5,143 +5,49 @@ const io = require('socket.io')(http);
 const Switches = require('../switch');
 
 const huejay = require('huejay');
-const credentials = require('../huebris');
-const hueClient = new huejay.Client(credentials);
+const huebris = require('../huebris');
+const hueClient = new huejay.Client(huebris.credentials);
 
 
 app.use(express.static('html'));
-app.get('/css/bulma.css', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/bulma/css/bulma.css');
-});
+app.use('/css', express.static(path.join(__dirname, '/bower_components/weather-icons/css')));
+app.use('/font', express.static(path.join(__dirname, '/bower_components/weather-icons/font')));
+app.use('/css/bulma.css', express.static(path.join(__dirname, '/node_modules/bulma/css/bulma.css')));
+
 
 io.on('connection', (socket) => {
     console.log('connected');
-    let switches = {
-        2: new Switches.GroupToggleSwitch({
-            name: 'Bedroom',
-            client: hueClient,
-            ids: [2],
-            defaults: {
-                brightness: 254,
-                hue: 41442,
-                saturation: 75,
-                on: true
-            },
-            callback: (item) => {
-                console.log('Bedroom callback called!');
-                socket.emit('finished', 'toggle', 2);
-            }
-        }),
-        6: new Switches.GroupToggleSwitch({
-            name: 'Living Room',
-            client: hueClient,
-            ids: [6],
-            defaults: {
-                brightness: 254,
-                hue: 41442,
-                saturation: 75,
-                on: true
-            },
-            callback: (item) => {
-                socket.emit('finished', 'toggle', 6);
-                console.log('Living Room callback called!');
-            }
-        })
-    };
-    let dimmers = {
-        2: new Switches.GroupDimmerSwitch({
-            name: 'Bedroom',
-            client: hueClient,
-            ids: [2],
-            callback: (item, argument) => {
-                console.log('Bedroom dimmer: '+argument);
-                socket.emit('finished', 'dimmer', 2);
-            }
-        }),
-        6: new Switches.GroupDimmerSwitch({
-            name: 'Bedroom',
-            client: hueClient,
-            ids: [6],
-            callback: (item, argument) => {
-                console.log('Living Room dimmer: '+argument);
-                socket.emit('finished', 'dimmer', 6);
-            }
-        })
-    };
-    let sceneCyclers = {
-        2: new Switches.GroupSceneCyclerSwitch({
-            name: 'Bedroom',
-            client: hueClient,
-            ids: [2],
-            // TODO: Make items' values retrieved at init?
-            items: [
-                "JYENfsx44Oha6OB",  // Read
-                "McLj5EnR5zIzsTz",  // Relax
-                "qHgAk9tHfL8pyst",  // Concentrate
-                "dRBxFnpi5iZLw8O",  // Nightlight
-                "7rpnhufF4XxkHOr",  // Energize
-                "FEsqa07hXbR-q8V",  // Bright
-                "3dFnrWm6q8Z6uYT"   // Dimmed
-            ],
-            callback: (item, argument) => {
-                console.log('Bedroom Scene Cycler called!');
-                socket.emit('finished', 'presets', 2);
-            }
-        }),
-        6: new Switches.GroupSceneCyclerSwitch({
-            name: 'Living Room',
-            client: hueClient,
-            ids: [6],
-            items: [
-                "TDC7v5PhcDwan8n",  // Read
-                "h0UA91WTpbHm-YF",  // Relax
-                "lx-NwIZlKYyiH7P",  // Concentrate
-                "iTsgT8bNmPqrVx3",  // Nightlight
-                "bqAXSBw78iuNdRh",  // Energize
-                "FpLMQSYYpnyJZIh",  // Bright
-                "1o3jcKS3kiCCwRW"   // Dimmed
-            ],
-            callback: (item, argument) => {
-                console.log('Living Room Scene Cycler called!');
-                socket.emit('finished', 'presets', 6);
-            }
-        })
-    };
 
-    socket.on('toggle', (group, action) => {
-        console.log('Toggle invoked. '+group+' | action: '+action);
-        let _switch = switches[group];
+    // TODO: Move this to a spot where it will run only once. Let the callback get socket passed in for immediate use.
+    let switches = {};
+    for (let group in huebris.switches) {
+        let groupSwitches = huebris.switches[group];
+        switches[group] = {};
+        for (let i in groupSwitches) {
+            let groupSwitch = groupSwitches[i];
+            try {
+                let {type, options} = groupSwitch;
+                options.client = hueClient;
+                options.callback = (item) => {
+                    console.log(`CALLBACK CALLED FOR (${group}, ${options.name})`);
+                    socket.emit('finished', group, options.name, item); // TODO: pass action back here; combine data into single object
+                };
+                switches[group][options.name] = new Switches[type](options);
+            } catch (err) {
+                console.log('ERROR INITIALIZING SWITCH FROM CONFIG: '+groupSwitch);
+                console.log(err);
+            }
+        }
+    }
+
+    socket.on('lightswitch', (data) => {
+        let {type, group, action} = data;
+        console.log(`Lightswitch invoked. TYPE: ${type} | GROUP: ${group} | ACTION: ${action}`);
+        let _switch = switches[group][type];
         _switch.exec(action)();
     });
-
-    socket.on('dimmer', (group, action) => {
-        console.log('Dimmer invoked. '+group+' | action: '+action);
-        let _switch = dimmers[group];
-        _switch.exec(action)();
-    });
-
-    socket.on('presets', (group, action) => {
-        console.log('Presets invoked. '+group+' | action: '+action);
-        let _switch = sceneCyclers[group];
-        _switch.exec(action)();
-    })
 });
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
-
-try {
-    let dashButtons = {
-        deskToggle: new Switches.LightDashButtonToggleSwitch({
-            name: 'Desk Toggle',
-            description: 'Nerf button',
-            client: hueClient,
-            mac: '18:74:2e:8b:84:ab',
-            ids: [8]
-        })
-    };
-} catch (err) {
-    console.log('DASH BUTTON ERROR! Probably wasn\'t run with sudo.');
-    console.log(err);
-}
